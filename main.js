d85bced0ca2aaa044277e3be3ed71ef75d1b7186
adamoc <oceallaighadam96@gmail.com>
2020-06-29 20:47:07 +0100

    Eigth Commit

--- main.js
+++ main.js
@@ -18,25 +18,25 @@ const scrollAnimation = () => {
     });
 }
 
-// const factsScrollAnimation = () => {
-//     const facts = [...document.querySelectorAll('.fact')],
-//     container = document.querySelector('.biggerContainer')
+const factsScrollAnimation = () => {
+    const facts = [...document.querySelectorAll('.fact')],
+    container = document.querySelector('.biggerContainer')
 
-//     console.log(facts , container)
-//     secondOne = gsap.to(facts, {
-//         xPercent: -100 * (facts.length - 1),
-//         ease: "none",
-//         scrollTrigger: {
-//             trigger: container,
-//             pin: true,
-//             scrub: 1,
-//             snap: 1 / (facts.length - 1),
-//             // base vertical scrolling on how wide the container is so it feels more natural.
-//             end: () => "+=" + container.offsetWidth
-//         }
-//     });
+    console.log(facts , container)
+    secondOne = gsap.to(facts, {
+        xPercent: -100 * (facts.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: container,
+            pin: true,
+            scrub: 1,
+            snap: 1 / (facts.length - 1),
+            // base vertical scrolling on how wide the container is so it feels more natural.
+            end: () => "+=" + container.offsetWidth
+        }
+    });
 
-// }
+}
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -46,9 +46,9 @@ if (typeof myAnim === "undefined") {
     scrollAnimation();
 } 
 
-// if(typeof secondOne === "undefined") {
-//     factsScrollAnimation()
-// }
+if(typeof secondOne === "undefined") {
+    factsScrollAnimation()
+}
 
 barba.init({
     sync: true,
@@ -61,9 +61,12 @@ barba.init({
         },
         async enter() {
             document.documentElement.scrollTop = 0;
+            barba.on('newPageReady', ()=> {
+                factsScrollAnimation();
+            })
             ScrollTrigger.refresh()
             setTimeout(scrollAnimation, 10);
-            // setTimeout(factsScrollAnimation, 10);
+            setTimeout(factsScrollAnimation, 10);
         },
     }],
 });

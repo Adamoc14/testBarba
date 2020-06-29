9d9feeabf4a489a13384de3c96da31b6a386ac1e
adamoc <oceallaighadam96@gmail.com>
2020-06-30 00:23:38 +0100

    Lost Count commit

--- main.js
+++ main.js
@@ -20,20 +20,11 @@ const scrollAnimation = () => {
 
 const yoBro = () => {
     const pages = [...document.querySelectorAll('.page')];
-    const container = document.querySelector(".container");
-
-    console.log(container, pages)
+    // const container = document.querySelector(".container");
+    console.log(pages)
     secondOne = gsap.to(pages, {
-        xPercent: -100 * (pages.length - 1),
-        ease: "none",
-        scrollTrigger: {
-            trigger: container,
-            pin: true,
-            scrub: 1,
-            snap: 1 / (pages.length - 1),
-            // base vertical scrolling on how wide the container is so it feels more natural.
-            end: () => "+=" + container.offsetWidth
-        }
+        background: "#ef4444",
+        stagger: .3
     });
 }
 

5316c024cdad85c5f72d185244ec210fe53834dd
adamoc <oceallaighadam96@gmail.com>
2020-06-29 12:48:51 +0100

    Fourth Commit

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
-let pages = [...document.querySelectorAll('.page')]
-gsap.to(pages, {
-    xPercent: -100 * (pages.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".container",
-        pin: true,
-        markers: true,
-        scrub: 1,
-        snap: 1 / (pages.length - 1),
-        // base vertical scrolling on how wide the container is so it feels more natural.
-        end: () => "+=" + document.querySelector(".container").offsetWidth
-    }
+const scrollAnimation = () => {
+    let pages = [...document.querySelectorAll('.page')]
+    gsap.to(pages, {
+        xPercent: -100 * (pages.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".container",
+            pin: true,
+            markers: true,
+            scrub: 1,
+            snap: 1 / (pages.length - 1),
+            // base vertical scrolling on how wide the container is so it feels more natural.
+            end: () => "+=" + document.querySelector(".container").offsetWidth
+        }
+    });
+}
+
+scrollAnimation()
+barba.init({
+    sync: true,
+    transitions: [{
+        name: 'transition-base',
+        async leave() {
+            const done = this.async();
+            await delay(1000);
+            done();
+        },
+        async enter() {
+            window.scrollTo(0, 0);
+        },
+    }],
+    views: [
+        {
+            namespace: 'about',
+            beforeEnter(data) {
+            },
+            afterEnter() {
+                scrollAnimation();
+            },
+        }
+    ],
 });

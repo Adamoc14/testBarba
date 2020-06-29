1f13d52cde6b15a73018c22dff1d3540d807278f
adamoc <oceallaighadam96@gmail.com>
2020-06-29 23:57:33 +0100

    Lost Count commit

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-let myAnim;
+let myAnim, secondOne
 const scrollAnimation = () => {
     const pages = [...document.querySelectorAll('.page')];
     const container = document.querySelector(".container");
 
     console.log(container, pages)
     myAnim = gsap.to(pages, {
+        xPercent: -100 * (pages.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: container,
+            pin: true,
+            scrub: 1,
+            snap: 1 / (pages.length - 1),
+            // base vertical scrolling on how wide the container is so it feels more natural.
+            end: () => "+=" + container.offsetWidth
+        }
+    });
+}
+
+const yoBro = () => {
+    const pages = [...document.querySelectorAll('.page')];
+    const container = document.querySelector(".container");
+
+    console.log(container, pages)
+    secondOne = gsap.to(pages, {
         xPercent: -100 * (pages.length - 1),
         ease: "none",
         scrollTrigger: {
@@ -25,6 +44,8 @@ function delay(ms) {
 if (typeof myAnim === "undefined") {
     scrollAnimation();
 }
+if(typeof secondOne === "undefined") 
+    yoBro()
 
 barba.init({
     sync: true,

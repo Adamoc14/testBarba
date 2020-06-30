32edcf0772487adf890a1388bb20111b50d6da2a
adamoc <oceallaighadam96@gmail.com>
2020-06-30 11:09:31 +0100

    Tesing out Barba With more trying to get irregulaity in markers

--- main.js
+++ main.js
@@ -1,33 +1,56 @@
-let myAnim , secondOne
-const scrollAnimation = () => {
-    const pages = [...document.querySelectorAll('.page')];
-    const container = document.querySelector(".container");
-    if (pages || container === null) return 
-    console.log(container, pages)
-    myAnim = gsap.to(pages, {
-        xPercent: -100 * (pages.length - 1),
-        ease: "none",
-        scrollTrigger: {
-            trigger: container,
-            pin: true,
-            scrub: 1,
-            snap: 1 / (pages.length - 1),
-            // base vertical scrolling on how wide the container is so it feels more natural.
-            end: () => "+=" + container.offsetWidth
-        }
-    });
+// Variable Declarations and Function Definitions 
+//____________________________________________________________________
+/**
+ * Homepage Variables
+ */
+
+/**
+ * About Page Variables
+ */
+let myAnim
+
+//____________________________________________________________________
+
+/**
+ * Global Page Functions
+ */
+
+/**
+ * Homepage Functions
+ */
+
+/**
+ * About Page Functions
+ */
+const aboutInit = () => {
+    console.log('We have lift off ')
+    const allClasses = [...document.querySelectorAll('[class]')]
+    console.log(allClasses.length)
+    // allClasses.map(classes => console.log(classes.className.split(' ')))
+    // .some(c => /gsap-.*/.test(c))
+    // console.log(allClasses , allClasses.length)
+    myAnim = undefined
+    if(typeof myAnim === "undefined") {
+        // setTimeout(scroll_facts_tl_func , 10)
+        horizontalScrollAnim();
+    }
+    // gsap.to(face , {
+    //     attr: { viewBox: face_viewBox },
+    //     duration: 3
+    // })
 }
 
-const yoBro = () => {
+const horizontalScrollAnim = () => {
     const facts = [...document.querySelectorAll('.fact')];
     console.log(facts)
 
-    secondOne = gsap.to(facts, {
+    myAnim = gsap.to(facts, {
         xPercent: -115 * (facts.length - 1),
         ease: "none",
         scrollTrigger: {
             trigger: ".factsContainer",
             pin: true,
+            markers: true,
             scrub: 1,
             snap: 1 / (facts.length - 1),
             // base vertical scrolling on how wide the container is so it feels more natural.
@@ -36,16 +59,16 @@ const yoBro = () => {
     });
 }
 
+//____________________________________________________________________
+
+// Helper Methods
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-if (typeof myAnim === "undefined" && [...document.querySelectorAll('.page')] !== []) {
-    scrollAnimation();
-}
-if(typeof secondOne === "undefined") 
-    setTimeout(yoBro , 10000)
+//____________________________________________________________________
 
+// Initialization Methods
 barba.init({
     sync: true,
     transitions: [{
@@ -57,16 +80,16 @@ barba.init({
         },
         async enter() {
             document.documentElement.scrollTop = 0;
-            setTimeout(scrollAnimation, 10);
-            // setTimeout(yoBro , 10)
         },
     }],
     views: [
         {
             namespace: 'about',
             afterEnter() {
-                // setTimeout(yoBro , 10) 
+                aboutInit()
             },
         }
     ],
 });
+
+//____________________________________________________________________

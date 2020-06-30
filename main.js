7be5301d1c50853c4b0a023a80ac1a2ace8b6fef
adamoc <oceallaighadam96@gmail.com>
2020-06-30 01:15:12 +0100

    Lost Count commit

--- main.js
+++ main.js
@@ -43,8 +43,8 @@ function delay(ms) {
 if (typeof myAnim === "undefined" && [...document.querySelectorAll('.page')] !== []) {
     scrollAnimation();
 }
-// if(typeof secondOne === "undefined") 
-//     yoBro()
+if(typeof secondOne === "undefined") 
+    yoBro()
 
 barba.init({
     sync: true,
@@ -65,7 +65,7 @@ barba.init({
         {
             namespace: 'about',
             enter() {
-                setTimeout(yoBro , 10) 
+                // setTimeout(yoBro , 10) 
             },
         }
     ],
